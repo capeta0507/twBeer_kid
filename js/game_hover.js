434535f48905d05fa7d75cb37b23527a59fc1bed
capeta0507 <nintendof1@gmail.com>
2020-08-03 20:17:25 +0800

    gender

---
+++ js/game_hover.js
@@ -0,0 +1,329 @@
+// hover事件
+$('.get-result').hover(function(){
+    $('.get-result img').attr("src", "img/makeResult_hover.png");
+},function(){
+    $('.get-result img').attr("src", "img/makeResult.png")
+});
+
+// 男
+$('.mq1_1').hover(function(){
+    $('.mq1_1 img').attr("src", "img/question/male/male_q101_hover.png");
+},function(){
+    $('.mq1_1 img').attr("src", "img/question/male/male_q101.png");
+});
+$('.mq1_2').hover(function(){
+    $('.mq1_2 img').attr("src", "img/question/male/male_q102_hover.png");
+},function(){
+    $('.mq1_2 img').attr("src", "img/question/male/male_q102.png");
+});
+$('.mq1_3').hover(function(){
+    $('.mq1_3 img').attr("src", "img/question/male/male_q103_hover.png");
+},function(){
+    $('.mq1_3 img').attr("src", "img/question/male/male_q103.png");
+});
+$('.mq1_4').hover(function(){
+    $('.mq1_4 img').attr("src", "img/question/male/male_q104_hover.png");
+},function(){
+    $('.mq1_4 img').attr("src", "img/question/male/male_q104.png");
+});
+$('.mq1_5').hover(function(){
+    $('.mq1_5 img').attr("src", "img/question/male/male_q105_hover.png");
+},function(){
+    $('.mq1_5 img').attr("src", "img/question/male/male_q105.png");
+});
+$('.mq1_6').hover(function(){
+    $('.mq1_6 img').attr("src", "img/question/male/male_q106_hover.png");
+},function(){
+    $('.mq1_6 img').attr("src", "img/question/male/male_q106.png");
+});
+
+$('.mq2_1').hover(function(){
+    $('.mq2_1 img').attr("src", "img/question/male/male_q201_hover.png");
+},function(){
+    $('.mq2_1 img').attr("src", "img/question/male/male_q201.png");
+});
+$('.mq2_2').hover(function(){
+    $('.mq2_2 img').attr("src", "img/question/male/male_q202_hover.png");
+},function(){
+    $('.mq2_2 img').attr("src", "img/question/male/male_q202.png");
+});
+$('.mq2_3').hover(function(){
+    $('.mq2_3 img').attr("src", "img/question/male/male_q203_hover.png");
+},function(){
+    $('.mq2_3 img').attr("src", "img/question/male/male_q203.png");
+});
+$('.mq2_4').hover(function(){
+    $('.mq2_4 img').attr("src", "img/question/male/male_q204_hover.png");
+},function(){
+    $('.mq2_4 img').attr("src", "img/question/male/male_q204.png");
+});
+$('.mq2_5').hover(function(){
+    $('.mq2_5 img').attr("src", "img/question/male/male_q205_hover.png");
+},function(){
+    $('.mq2_5 img').attr("src", "img/question/male/male_q205.png");
+});
+$('.mq1_6').hover(function(){
+    $('.mq1_6 img').attr("src", "img/question/male/male_q206_hover.png");
+},function(){
+    $('.mq1_6 img').attr("src", "img/question/male/male_q206.png");
+});
+
+$('.mq3_1').hover(function(){
+    $('.mq3_1 img').attr("src", "img/question/male/male_q301_hover.png");
+},function(){
+    $('.mq3_1 img').attr("src", "img/question/male/male_q301.png");
+});
+$('.mq3_2').hover(function(){
+    $('.mq3_2 img').attr("src", "img/question/male/male_q302_hover.png");
+},function(){
+    $('.mq3_2 img').attr("src", "img/question/male/male_q302.png");
+});
+$('.mq3_3').hover(function(){
+    $('.mq3_3 img').attr("src", "img/question/male/male_q303_hover.png");
+},function(){
+    $('.mq3_3 img').attr("src", "img/question/male/male_q303.png");
+});
+$('.mq3_4').hover(function(){
+    $('.mq3_4 img').attr("src", "img/question/male/male_q304_hover.png");
+},function(){
+    $('.mq3_4 img').attr("src", "img/question/male/male_q304.png");
+});
+$('.mq3_5').hover(function(){
+    $('.mq3_5 img').attr("src", "img/question/male/male_q305_hover.png");
+},function(){
+    $('.mq3_5 img').attr("src", "img/question/male/male_q305.png");
+});
+$('.mq3_6').hover(function(){
+    $('.mq3_6 img').attr("src", "img/question/male/male_q306_hover.png");
+},function(){
+    $('.mq3_6 img').attr("src", "img/question/male/male_q306.png");
+});
+
+$('.mq4_1').hover(function(){
+    $('.mq4_1 img').attr("src", "img/question/male/male_q401_hover.png");
+},function(){
+    $('.mq4_1 img').attr("src", "img/question/male/male_q401.png");
+});
+$('.mq4_2').hover(function(){
+    $('.mq4_2 img').attr("src", "img/question/male/male_q402_hover.png");
+},function(){
+    $('.mq4_2 img').attr("src", "img/question/male/male_q402.png");
+});
+$('.mq4_3').hover(function(){
+    $('.mq4_3 img').attr("src", "img/question/male/male_q403_hover.png");
+},function(){
+    $('.mq4_3 img').attr("src", "img/question/male/male_q403.png");
+});
+$('.mq4_4').hover(function(){
+    $('.mq4_4 img').attr("src", "img/question/male/male_q404_hover.png");
+},function(){
+    $('.mq4_4 img').attr("src", "img/question/male/male_q404.png");
+});
+$('.mq4_5').hover(function(){
+    $('.mq4_5 img').attr("src", "img/question/male/male_q405_hover.png");
+},function(){
+    $('.mq4_5 img').attr("src", "img/question/male/male_q405.png");
+});
+$('.mq4_6').hover(function(){
+    $('.mq4_6 img').attr("src", "img/question/male/male_q406_hover.png");
+},function(){
+    $('.mq4_6 img').attr("src", "img/question/male/male_q406.png");
+});
+
+$('.mq5_1').hover(function(){
+    $('.mq5_1 img').attr("src", "img/question/male/male_q501_hover.png");
+},function(){
+    $('.mq5_1 img').attr("src", "img/question/male/male_q501.png");
+});
+$('.mq5_2').hover(function(){
+    $('.mq5_2 img').attr("src", "img/question/male/male_q502_hover.png");
+},function(){
+    $('.mq5_2 img').attr("src", "img/question/male/male_q502.png");
+});
+$('.mq5_3').hover(function(){
+    $('.mq5_3 img').attr("src", "img/question/male/male_q503_hover.png");
+},function(){
+    $('.mq5_3 img').attr("src", "img/question/male/male_q503.png");
+});
+$('.mq5_4').hover(function(){
+    $('.mq5_4 img').attr("src", "img/question/male/male_q504_hover.png");
+},function(){
+    $('.mq5_4 img').attr("src", "img/question/male/male_q504.png");
+});
+$('.mq5_5').hover(function(){
+    $('.mq5_5 img').attr("src", "img/question/male/male_q505_hover.png");
+},function(){
+    $('.mq5_5 img').attr("src", "img/question/male/male_q505.png");
+});
+$('.mq5_6').hover(function(){
+    $('.mq5_6 img').attr("src", "img/question/male/male_q506_hover.png");
+},function(){
+    $('.mq5_6 img').attr("src", "img/question/male/male_q506.png");
+});
+
+// 女
+$('.fq1_1').hover(function(){
+    $('.fq1_1 img').attr("src", "img/question/female/female_q101_hover.png");
+},function(){
+    $('.fq1_1 img').attr("src", "img/question/female/female_q101.png");
+});
+$('.fq1_2').hover(function(){
+    $('.fq1_2 img').attr("src", "img/question/female/female_q102_hover.png");
+},function(){
+    $('.fq1_2 img').attr("src", "img/question/female/female_q102.png");
+});
+$('.fq1_3').hover(function(){
+    $('.fq1_3 img').attr("src", "img/question/female/female_q103_hover.png");
+},function(){
+    $('.fq1_3 img').attr("src", "img/question/female/female_q103.png");
+});
+$('.fq1_4').hover(function(){
+    $('.fq1_4 img').attr("src", "img/question/female/female_q104_hover.png");
+},function(){
+    $('.fq1_4 img').attr("src", "img/question/female/female_q104.png");
+});
+$('.fq1_5').hover(function(){
+    $('.fq1_5 img').attr("src", "img/question/female/female_q105_hover.png");
+},function(){
+    $('.fq1_5 img').attr("src", "img/question/female/female_q105.png");
+});
+$('.fq1_6').hover(function(){
+    $('.fq1_6 img').attr("src", "img/question/female/female_q106_hover.png");
+},function(){
+    $('.fq1_6 img').attr("src", "img/question/female/female_q106.png");
+});
+
+$('.fq2_1').hover(function(){
+    $('.fq2_1 img').attr("src", "img/question/female/female_q201_hover.png");
+},function(){
+    $('.fq2_1 img').attr("src", "img/question/female/female_q201.png");
+});
+$('.fq2_2').hover(function(){
+    $('.fq2_2 img').attr("src", "img/question/female/female_q202_hover.png");
+},function(){
+    $('.fq2_2 img').attr("src", "img/question/female/female_q202.png");
+});
+$('.fq2_3').hover(function(){
+    $('.fq2_3 img').attr("src", "img/question/female/female_q203_hover.png");
+},function(){
+    $('.fq2_3 img').attr("src", "img/question/female/female_q203.png");
+});
+$('.fq2_4').hover(function(){
+    $('.fq2_4 img').attr("src", "img/question/female/female_q204_hover.png");
+},function(){
+    $('.fq2_4 img').attr("src", "img/question/female/female_q204.png");
+});
+$('.fq2_5').hover(function(){
+    $('.fq2_5 img').attr("src", "img/question/female/female_q205_hover.png");
+},function(){
+    $('.fq2_5 img').attr("src", "img/question/female/female_q205.png");
+});
+$('.fq2_6').hover(function(){
+    $('.fq2_6 img').attr("src", "img/question/female/female_q206_hover.png");
+},function(){
+    $('.fq2_6 img').attr("src", "img/question/female/female_q206.png");
+});
+
+$('.fq3_1').hover(function(){
+    $('.fq3_1 img').attr("src", "img/question/female/female_q301_hover.png");
+},function(){
+    $('.fq3_1 img').attr("src", "img/question/female/female_q301.png");
+});
+$('.fq3_2').hover(function(){
+    $('.fq3_2 img').attr("src", "img/question/female/female_q302_hover.png");
+},function(){
+    $('.fq3_2 img').attr("src", "img/question/female/female_q302.png");
+});
+$('.fq3_3').hover(function(){
+    $('.fq3_3 img').attr("src", "img/question/female/female_q303_hover.png");
+},function(){
+    $('.fq3_3 img').attr("src", "img/question/female/female_q303.png");
+});
+$('.fq3_4').hover(function(){
+    $('.fq3_4 img').attr("src", "img/question/female/female_q304_hover.png");
+},function(){
+    $('.fq3_4 img').attr("src", "img/question/female/female_q304.png");
+});
+$('.fq3_5').hover(function(){
+    $('.fq3_5 img').attr("src", "img/question/female/female_q305_hover.png");
+},function(){
+    $('.fq3_5 img').attr("src", "img/question/female/female_q305.png");
+});
+$('.fq3_6').hover(function(){
+    $('.fq3_6 img').attr("src", "img/question/female/female_q306_hover.png");
+},function(){
+    $('.fq3_6 img').attr("src", "img/question/female/female_q306.png");
+});
+
+$('.fq4_1').hover(function(){
+    $('.fq4_1 img').attr("src", "img/question/female/female_q401_hover.png");
+},function(){
+    $('.fq4_1 img').attr("src", "img/question/female/female_q401.png");
+});
+$('.fq4_2').hover(function(){
+    $('.fq4_2 img').attr("src", "img/question/female/female_q402_hover.png");
+},function(){
+    $('.fq4_2 img').attr("src", "img/question/female/female_q402.png");
+});
+$('.fq4_3').hover(function(){
+    $('.fq4_3 img').attr("src", "img/question/female/female_q403_hover.png");
+},function(){
+    $('.fq4_3 img').attr("src", "img/question/female/female_q403.png");
+});
+$('.fq4_4').hover(function(){
+    $('.fq4_4 img').attr("src", "img/question/female/female_q404_hover.png");
+},function(){
+    $('.fq4_4 img').attr("src", "img/question/female/female_q404.png");
+});
+$('.fq4_5').hover(function(){
+    $('.fq4_5 img').attr("src", "img/question/female/female_q405_hover.png");
+},function(){
+    $('.fq4_5 img').attr("src", "img/question/female/female_q405.png");
+});
+$('.fq4_6').hover(function(){
+    $('.fq4_6 img').attr("src", "img/question/female/female_q406_hover.png");
+},function(){
+    $('.fq4_6 img').attr("src", "img/question/female/female_q406.png");
+});
+
+$('.fq5_1').hover(function(){
+    $('.fq5_1 img').attr("src", "img/question/female/female_q501_hover.png");
+},function(){
+    $('.fq5_1 img').attr("src", "img/question/female/female_q501.png");
+});
+$('.fq5_2').hover(function(){
+    $('.fq5_2 img').attr("src", "img/question/female/female_q502_hover.png");
+},function(){
+    $('.fq5_2 img').attr("src", "img/question/female/female_q502.png");
+});
+$('.fq5_3').hover(function(){
+    $('.fq5_3 img').attr("src", "img/question/female/female_q503_hover.png");
+},function(){
+    $('.fq5_3 img').attr("src", "img/question/female/female_q503.png");
+});
+$('.fq5_4').hover(function(){
+    $('.fq5_4 img').attr("src", "img/question/female/female_q504_hover.png");
+},function(){
+    $('.fq5_4 img').attr("src", "img/question/female/female_q504.png");
+});
+$('.fq5_5').hover(function(){
+    $('.fq5_5 img').attr("src", "img/question/female/female_q505_hover.png");
+},function(){
+    $('.fq5_5 img').attr("src", "img/question/female/female_q505.png");
+});
+$('.fq5_6').hover(function(){
+    $('.fq5_6 img').attr("src", "img/question/female/female_q506_hover.png");
+},function(){
+    $('.fq5_6 img').attr("src", "img/question/female/female_q506.png");
+});
+// 分享
+$('.share-result').hover(function(){
+    $('.share-result img').attr("src", "img/fb_share_hover.png");
+},function(){
+    $('.share-result img').attr("src", "img/fb_share.png");
+});
+$('.restart').hover(function(){
+    $('.restart img').attr("src", "img/restart_hover.png");
+},function(){
+    $('.restart img').attr("src", "img/restart.png");
+});
